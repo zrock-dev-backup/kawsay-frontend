641580895950f90846421e45ea0e73d1094a34ef
Santiago C. Saavedra <zrock_git@zohomail.com>
2025-04-18 03:12:33 -0400

    chore: kawsay landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Box, Button, Typography} from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>This is a test</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Box sx={{width: '100%', maxWidth: 500}}>
+                <Typography variant="h2" gutterBottom>
+                    Kawsay timetable management
+                </Typography>
+                <Button variant={"contained"} href={"/creation"}>
+                    New timetable
+                </Button>
+            </Box>
+        </>
+    )
 }
 
 export default App
